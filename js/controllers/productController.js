7bf3c9465872896be5481f14c3e08536e0df20ba
thuongph22411ca <thuongph22411ca@st.uel.edu.vn>
2025-03-24 15:07:58 +0700

    Update productController.js

--- js/controllers/productController.js
+++ js/controllers/productController.js
@@ -71,7 +71,7 @@ app.controller("ProductController", [
         .then((response) => {
           $scope.$applyAsync(() => {
             const relativeUrl = response.data.url;
-            const fullUrl = response.data.url; 
+            const fullUrl = `http://127.0.0.1:3001${relativeUrl}`;
             $scope.formData.image = fullUrl;
           });
         })
